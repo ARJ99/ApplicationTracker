eaa88171ee52a459b47065476a8c3b7cef9ca373
PARIS <alejoriios14@gmail.com>
2026-02-10 10:09:11 -0500

    feat: Remove Navbar buttons after signing in

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,13 @@
 import { Briefcase } from "lucide-react"
 import Link from "next/link"
 import { Button } from "./ui/button"
+import { getSession } from "@/lib/auth/auth"
 
 
-export const Navbar = () => {
+export const Navbar = async () => {
+
+    const session = await getSession();
+
     return (
         <nav className="border-b border-gray-200 bg-white">
             <div className="container mx-auto flex justify-between h-16 items-center px-4">
@@ -12,12 +16,21 @@ export const Navbar = () => {
                     Job Tracker
                 </Link>
                 <div className="flex items-center gap-4">
-                    <Link href="/sign-in" >
-                        <Button variant='ghost' className="text-gray-700 hover:text-black">Log in</Button>
-                    </Link>
-                    <Link href="/sign-up">
-                        <Button className="bg-primary hover:bg-primary/90">Start for free</Button>
-                    </Link>
+                    {session?.user ? (
+                        <>
+                            <Link href="/dashboard">
+                                <Button variant="ghost" className="text-gray-700 hover:text-black">Dashboard</Button>
+                            </Link>
+                        </>
+                    ) : (
+                        <>
+                            <Link href="/sign-in" >
+                                <Button variant='ghost' className="text-gray-700 hover:text-black">Log in</Button>
+                            </Link>
+                            <Link href="/sign-up">
+                                <Button className="bg-primary hover:bg-primary/90">Start for free</Button>
+                            </Link>
+                        </>)}
 
                 </div>
             </div>
