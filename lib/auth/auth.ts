ae3a73b73fe4133df3835ebdd3cd9367a8e1a3db
PARIS <alejoriios14@gmail.com>
2026-02-14 10:27:10 -0500

    Feat: Models in MDB

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -3,6 +3,7 @@ import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { MongoClient } from "mongodb";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { initializeUserBoard } from "../init-user-board";
 
 const client = new MongoClient(process.env.MONGODB_URI!);
 const db = client.db();
@@ -11,20 +12,30 @@ export const auth = betterAuth({
     database: mongodbAdapter(db, {
         client,
     }),
-    emailAndPassword:{
+    emailAndPassword: {
         enabled: true,
-    
+    },
+    databaseHooks: {
+        user: {
+            create: {
+                after: async (user) => {
+                    if (user.id) {
+                        await initializeUserBoard(user.id);
+                    }
+                }
+            }
+        }
     }
 });
 
-export async function getSession(){
+export async function getSession() {
     const result = await auth.api.getSession({
         headers: await headers(),
     });
     return result;
 }
 
-export async function signOut(){
+export async function signOut() {
     const result = await auth.api.signOut({
         headers: await headers(),
     });
