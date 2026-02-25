c7b05600bf82c0dac1400daa97b8851e5343ba9a
PARIS <alejoriios14@gmail.com>
2026-02-25 10:00:27 -0500

    Feat: Dashboard Cached

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,27 +1,42 @@
+
+
 import { KanbanBoard } from "@/components/kanban-board";
 import { getSession } from "@/lib/auth/auth"
 import connectDB from "@/lib/db";
 import { Board } from "@/lib/models";
 import { redirect } from "next/navigation";
+import { Suspense } from "react";
 
-
-const Dashboard = async () => {
-    const session = await getSession();
-    if (!session?.user){
-        redirect("sign-in");
-    }
+async function getBoard(userId: string) {
+    "use cache"
 
     await connectDB();
 
-    const board = await Board.findOne({
-        userId: session.user.id,
-        name:"Job Hunt",
+    const boardDoc = await Board.findOne({
+        userId: userId,
+        name: "Job Hunt",
     }).populate({
-        path:"columns",
-        populate:{
-            path:"jobApplications",
+        path: "columns",
+        populate: {
+            path: "jobApplications",
         }
-    })
+    });
+
+    if(!boardDoc) return null;
+    const board = JSON.parse(JSON.stringify(boardDoc));
+
+    return board;
+};
+
+async function DashboardPage() {
+    const session = await getSession();
+    const board = await getBoard(session?.user.id ?? "");
+
+    if (!session?.user) {
+        redirect("sign-in");
+    }
+
+
 
     return (
         <div className="min-h-screen bg-white">
@@ -30,9 +45,17 @@ const Dashboard = async () => {
                     <h1 className="text-3xl font-bold text-black">Job Hunt</h1>
                     <p className="text-gray-600">Track your job application</p>
                 </div>
-                <KanbanBoard board={JSON.parse(JSON.stringify(board))} userId={session.user.id}/>
+                <KanbanBoard board={board} userId={session.user.id} />
             </div>
         </div>
+    )
+}
+
+const Dashboard = async () => {
+    return (
+        <Suspense fallback={<p>Loading ...</p>}>
+            <DashboardPage />
+        </Suspense>
     )
 }
 
